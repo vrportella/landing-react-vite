6d0aa9806cb4fdf038a49d8a7748d9ce0afafd48
Valeria Cuellar Portella <valeria.cuellar@itsight.pe>
2023-09-13 02:10:40 -0500

    vrc

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://vrcportella.gitlab.io/landing-react-vite'
+  base: 'https://vrcportella.github.io/landing-react-vite'
 })
